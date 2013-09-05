cfb72bef8e1f1c07f88efc68d25c643a71e0e2a9
ChengDuo <cd19900419@hotmail.com>
2013-09-05 23:51:45 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bbwifi::Application.config.secret_token = '3e6dc924ad7d924da3d095cbc519bf961ed406f5f250b7a01b31fedc483061da6f5220338f87132e84d59164cdaab03de8cb9fb9b66ae9417c7f040ac3066cc1'
